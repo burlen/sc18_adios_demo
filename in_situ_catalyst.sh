6efeaf0e5bbcef94948eb6c300aa16dcff8f29ad
Burlen Loring <bloring@lbl.gov>
2018-11-09 09:33:05 -0800

    bring back in situ demo cori

---
+++ in_situ_catalyst.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+n=4
+b=64
+dt=0.05
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+bld=`echo -e '\e[1m'`
+red=`echo -e '\e[31m'`
+grn=`echo -e '\e[32m'`
+blu=`echo -e '\e[36m'`
+wht=`echo -e '\e[0m'`
+
+mkdir -p ./configs
+cp ${script_dir}/configs/* ./configs
+
+echo "+ module use /usr/common/software/sensei/modulefiles"
+module use /usr/common/software/sensei/modulefiles
+
+echo "+ module load sensei/2.1.0-catalyst-shared"
+module load sensei/2.1.0-catalyst-shared
+
+set -x
+
+cat ./configs/random_2d_${b}_catalyst.xml | sed "s/.*/$blu&$wht/"
+
+srun -n ${n} \
+    oscillator -b ${n} -t ${dt} -s ${b},${b},1 -g 1 -p 0 \
+    -f ./configs/random_2d_${b}_catalyst.xml \
+    ./configs/random_2d_${b}.osc 2>&1 | sed "s/.*/$red&$wht/"
