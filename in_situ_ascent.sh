b0886bd965f4788c5129c34f25d61f65fd25a71e
sensei user <sensei-sc19@dev.null>
2019-11-12 15:12:07 -0800

    added ascent demos

---
+++ in_situ_ascent.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+n=4
+b=64
+dt=0.25
+bld=`echo -e '\e[1m'`
+red=`echo -e '\e[31m'`
+grn=`echo -e '\e[32m'`
+blu=`echo -e '\e[36m'`
+wht=`echo -e '\e[0m'`
+
+echo "+ module load sensei/3.1.0-ascent-shared"
+module load sensei/3.1.0-ascent-shared
+
+set -x
+
+export OMP_NUM_THREADS=2
+
+cat ./configs/random_2d_${b}_ascent.xml | sed "s/.*/$blu&$wht/"
+
+mpiexec -n ${n} \
+    oscillator -b ${n} -t ${dt} -s ${b},${b},1 -g 1 -p 0 \
+    -f ./configs/random_2d_${b}_ascent.xml \
+    ./configs/random_2d_${b}.osc 2>&1 | sed "s/.*/$red&$wht/"
---
+++ in_transit_ascent.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+n=4
+b=64
+dt=0.5
+delay=1
+max_delay=100
+file=random_2d_${b}.bp
+bld=`echo -e '\e[1m'`
+red=`echo -e '\e[31m'`
+grn=`echo -e '\e[32m'`
+blu=`echo -e '\e[36m'`
+wht=`echo -e '\e[0m'`
+
+echo "+ module load sensei/3.1.0-ascent-shared"
+module load sensei/3.1.0-ascent-shared
+
+set -x
+
+cat ./configs/random_2d_${b}_adios1_write.xml | sed "s/.*/$blu&$wht/"
+
+mpiexec -n ${n} \
+    oscillator -b ${n} -t ${dt} -s ${b},${b},1  -p 0 -g 1 \
+    -f ./configs/random_2d_${b}_adios1_write.xml \
+    ./configs/random_2d_${b}.osc 2>&1 | sed "s/.*/$red&$wht/" &
+
+cat ./configs/random_2d_${b}_adios1_read.xml | sed "s/.*/$blu&$wht/"
+cat ./configs/random_2d_${b}_ascent.xml | sed "s/.*/$blu&$wht/"
+
+export OMP_NUM_THREADS=2
+
+mpiexec -n ${n} \
+    SENSEIEndPoint -t ./configs/random_2d_${b}_adios1_read.xml \
+    -a ./configs/random_2d_${b}_ascent.xml 2>&1 | sed "s/.*/$grn&$wht/"
